cf4036720b55882016f4df0def0c8cec82a02b04
h4nie <hniazd@gmail.com>
2022-05-01 23:11:39 -0400

    added GitHub link

--- src/App.js
+++ src/App.js
@@ -6,6 +6,12 @@ function App() {
     <div className="App">
       <div className="container">
         <Weather />
+        <p>
+          This project was coded by H. Azad and is{" "}
+          <a href="https://github.com/h4nie/react-weather-app">
+            open-sourced on GitHub
+          </a>
+        </p>
       </div>
     </div>
   );
